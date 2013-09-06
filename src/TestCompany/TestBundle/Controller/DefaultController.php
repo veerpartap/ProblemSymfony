4c8056684d82fae8c87fdca39d2d0dda109c34cf
Veerpartap singh <veer@imentor.dk>
2013-09-06 15:32:41 +0200

    New Bundle with TestCompany namespace is added

---
+++ src/TestCompany/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TestCompany\TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TestCompanyTestBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/TestCompany/TestBundle/TestCompanyTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TestCompany\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TestCompanyTestBundle extends Bundle
+{
+}
