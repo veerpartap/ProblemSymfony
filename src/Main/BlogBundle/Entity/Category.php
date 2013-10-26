162a33df51aa7066ac63b06f630e7c2f73f9e1f1
Veerpartap singh <veer@imentor.dk>
2013-10-26 13:25:17 +0200

    Symfony Problem

---
+++ src/Main/BlogBundle/Entity/Category.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Main\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var array
+     */
+    protected $products;
+
+    public function __construct()
+    {
+        //$this->products = new ArrayCollection();
+        $this->products[] = "";
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add products
+     *
+     * @param \Main\BlogBundle\Entity\Product $products
+     * @return Category
+     */
+    public function addProduct(\Main\BlogBundle\Entity\Product $products)
+    {
+        $this->products[] = $products;
+    
+        return $this;
+    }
+
+    /**
+     * Remove products
+     *
+     * @param \Main\BlogBundle\Entity\Product $products
+     */
+    public function removeProduct(\Main\BlogBundle\Entity\Product $products)
+    {
+        $this->products->removeElement($products);
+    }
+
+    /**
+     * Get products
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+}
---
+++ src/Main/BlogBundle/Form/ProductType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Main\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProductType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('category')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Main\BlogBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'main_blogbundle_product';
+    }
+}
